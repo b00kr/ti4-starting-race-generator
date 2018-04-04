898de3385b3661e87aca8960e76070341e628de7
Daryl Granger <darylgranger@gmail.com>
2018-04-04 13:59:23 -0700

    make it better

---
+++ ti4.js
@@ -0,0 +1,61 @@
+
+document.querySelector("button").addEventListener("click", function(){
+
+  var raceList = [
+    "Emirates of Hacan",
+    "Universities of Jol-Nar",
+    "L1Z1X Mindnet",
+    "Naalu Collective",
+    "Federation of Sol",
+    "Barony of Letnev",
+    "Mentak Coalition",
+    "The Arborec",
+    "Yssaril Tribes",
+    "Sardakk N’or",
+    "Xxcha Kingdom",
+    "Clan of Saar",
+    "Brotherhood of Yin",
+    "The Winnu",
+    "Embers of Muatt",
+    "Ghosts of Creuss",
+    "Nekro Virus"
+    ]
+
+    var players = Number(document.getElementById("playerCount").value);
+    var races = Number(document.getElementById("raceCount").value);
+
+    document.getElementById("output").innerHTML = "";
+
+    if (players>6 || (players*races>17) || players < 3) {
+
+      var newp = document.createElement("p").innerText = ("invalid configuration.  There must be 3 - 6 players and no more than 17 races.");
+      document.getElementById("output").append(newp);
+
+    } else {
+
+      for (var i = 0; i < players; i++) {
+
+          var newul = document.createElement("ul");
+          var newh3 = document.createElement("h3");
+
+          newh3.innerText = ("Player " + (i+1));
+
+          document.getElementById("output").append(newh3);
+          document.getElementById("output").append(newul);
+
+              for (var v = 0; v < races; v++) {
+
+                  var num = Math.floor((Math.random() * (raceList.length)))
+                  var newli = document.createElement("li");
+                  var ulList = document.getElementsByTagName("ul");
+                  var currentUl = ulList[ulList.length - 1]
+
+                  newli.innerText = (raceList[num]);
+
+                  currentUl.append(newli);
+
+                  raceList.splice(num, 1);
+              }
+      };
+    }
+  });

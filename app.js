7d37688e21344a6116b54304f5af2bef9ab0cf1f
Daryl Granger <darylgranger@gmail.com>
2018-07-22 11:43:19 -0700

    one more time!

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var port = process.env.PORT || 8000;
 
 
 
-app.get("/", function(req, res){
-  res.sendFile("./ti4_generator.html");
-})
+// app.get("/", function(req, res){
+//   res.sendFile("./ti4_generator.html");
+// })
 
+
+app.get("/", function(req, res)
+{
+    res.sendFile("ti4_generator.html", {"root": __dirname});
+});
 
 app.listen(port, function() {
     console.log("App is running on port " + port);

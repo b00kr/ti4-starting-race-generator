b1de2d72727cc0670cc3e6d0e0772ad4be7b518c
Daryl Granger <darylgranger@gmail.com>
2018-07-22 11:15:48 -0700

    configure listener for heroku

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 8000
 
 
-app.listen(3000, function(){
-  console.log("the server is listening on port 3000")
-});
 
 app.get("/", function(req, res){
   res.sendFile(ti4_generator.html)
 })
+
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
